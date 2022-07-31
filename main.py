87218ca583501b4c7d8dc056de42f2cb1937b816
dhriti-bharadwaj <110336997+dhriti-bharadwaj@users.noreply.github.com>
2022-07-31 18:15:46 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+from BookAccomodation.Interface import AppInterface as Interface
+
+app = Interface()                 ## Instatiation of Class AppInterface in module Interface.py
+app.mainloop()                    ## Activate GUI Form
